55d50f6aa83a85a33828853f86b47b9c9fba6e04
Abdulla Omar <aabdula2712@gmail.com>
2023-06-23 02:22:11 +0300

    AAddc

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,10 +8,9 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-<<<<<<< HEAD
 /*while (i < 10)*/
 /*{*/
-/*putchar(i); */
+/*putchar(i);*/
 /*}*/
 printf("Infinite loop avoided! \\o/\n");
 return (0);
