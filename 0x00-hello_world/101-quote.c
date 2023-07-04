beb7f70e8f2c5972c3d02eda6ec43fb89bad9eb3
Abdulla Omar <aabdula2712@gmail.com>
2023-07-04 08:40:21 +0300

    MOst 0f tests

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+write("echo aa");
+return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*this print the size of vars*/
+int main(void)
+{
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+printf("Size of a char: %zu byte(s)\n", sizeof(c));
+printf("Size of an int: %li byte(s)\n", sizeof(i));
+printf("Size of a long int: %li byte(s)\n", sizeof(li));
+printf("Size of a long long int: %li byte(s)\n", sizeof(lli));
+printf("Size of a float: %li byte(s)\n", sizeof(f));
+return (0);
+}
