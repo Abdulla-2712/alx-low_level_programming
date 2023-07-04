02920c6b325336ee3996f03a7bd0c9fc2570d437
Abdulla Omar <aabdula2712@gmail.com>
2023-07-04 08:55:14 +0300

    qw

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-/*This program prints a sentence*/
+/*
+ *main entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
