ff59860422b73e313ae5a514b779641ba9e21efc
Abdulla Omar <aabdula2712@gmail.com>
2023-07-15 11:26:27 +0300

    111

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - entry
+ *Return: =0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+	putchar(i);
+	i++;
+	}
+	printf("\n");
+	return (0);
+}
