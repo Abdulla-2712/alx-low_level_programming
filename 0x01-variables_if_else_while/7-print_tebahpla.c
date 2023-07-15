a5e64960eee5798fd8d9ce804951882610e6d3a5
Abdulla Omar <aabdula2712@gmail.com>
2023-07-15 14:12:36 +0300

    aaa

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - entry
+ *Return: =0
+ */
+int main(void)
+{
+	char a = 122;
+
+	while (a > 96)
+	{
+	putchar(a);
+	a--;
+	}
+	printf("\n");
+	return (0);
+}
