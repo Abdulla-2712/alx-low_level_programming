7060fc0040db0623d62346da8ead44a3d2c8a166
Abdulla Omar <aabdula2712@gmail.com>
2023-07-15 08:20:18 +0300

    asd

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - entry
+ *Return: =0
+ */
+int main(void)
+{
+	char a = 97;
+
+	while (a <= 123)
+	{
+	putchar(a);
+	a++;
+	}
+	return (0);
+}
