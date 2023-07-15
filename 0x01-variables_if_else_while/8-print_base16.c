83f2b24109f40b8722b5a0f813826fd429ce0e19
Abdulla Omar <aabdula2712@gmail.com>
2023-07-15 14:41:51 +0300

    111

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - entry
+ *Return: =0
+ */
+int main(void)
+{
+	for (int i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (int i = 97; i <= 102; i++)
+		putchar(i);
+	purchar('\n');
+	return (0);
+}
