b6cbcd7a8e0279536c98cbe80a568cdf7a7beb3d
Abdulla Omar <aabdula2712@gmail.com>
2023-07-20 17:45:38 +0300

    laaaaast

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -24,11 +24,11 @@ int main(void)
 						putchar(x);
 						putchar(y);
 						putchar(z);
-					}
 					if (x != '7' || y != '8' || z != '9')
 					{
 						putchar(',');
 						putchar(' ');
+					}
 					}
 					z++;
 				}
