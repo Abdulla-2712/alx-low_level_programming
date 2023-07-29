34234e07eaa0dc164006aaba4801b32ceff8a36a
Abdulla Omar <aabdula2712@gmail.com>
2023-07-29 03:32:19 +0300

    Ddd

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0;
+	size_t node_count = 0;
 
-    while (h)
-    {
-        if (!h->str)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
+	while (h)
+	{
+		if (!h->str)
+		printf("[0] (nil)\n");
+		else
+		printf("[%u] %s\n", h->len, h->str);
 
-        h = h->next;
-        node_count++;
-    }
+		h = h->next;
+		node_count++;
+	}
 
-    return (node_count);
+	return (node_count);
 }
